cf128dbcef27a56c5874139ec1f8052f09f26e2a
rawan-log <omranr725@gmail.com>
2026-08-28 00:53:45 +0300

    Update of my practice

--- practice.sql
+++ practice.sql
@@ -1,5 +1,33 @@
-UPDATE employees
-SET hourly_paid=10.25
+/*COMMIT;
+/*ROLLBACK;
+/*DELETE FROM employees;
+/*SET AUTOCOMMIT=OFF;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*DELETE FROM employees 
+WHERE employee_id=6;
+
+
+
+/*UPDATE employees
+SET hourly_paid=10.50;
+
+
+/*UPDATE employees
+SET hourly_paid=NULL,
+       hire_date="2023-01-07"
 WHERE employee_id=6;
 
 
