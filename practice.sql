e365bd0229e71f0c5bda700936d022aecca92ad6
rawan-log <omranr725@gmail.com>
2026-08-28 00:28:49 +0300

    clean structure

---
+++ practice.sql
@@ -0,0 +1,34 @@
+UPDATE employees
+SET hourly_paid=10.25
+WHERE employee_id=6;
+
+
+
+
+/*SELECT *
+FROM employees
+WHERE hire_date IS NULL; or IS NOT NULL 
+
+
+
+/*SELECT *
+FROM employees
+WHERE hourly_paid>=15;
+
+
+
+
+
+
+
+/*INSERT INTO employees (employee_id,first_name,last_name)
+/*VALUES(6,"Sheldon","Plankton");*/
+/*VALUES (1,"EUGENE","Krabs","krab@gmail.com",25.50,"2023-01-02");*/
+/*VALUES (2,"Squidward","Tentacles","squidward@gmail.com",15.00,"2023-01-03"),(3,"Spongebob","Squarepants","spongebob@gmail.com",15.00,"2023-01-03");*/
+
+
+
+
+SELECT * FROM employees;
+
+
